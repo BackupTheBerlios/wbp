e60379727ecbc22c4a52f9130123c21bd1e1cb8b
silcan <silcan>
2001-05-20 20:33:10 +0000

    *** empty log message ***

--- cgi-bin/tables.sql
+++ cgi-bin/tables.sql
@@ -9,6 +9,7 @@ create table wbp_user (
 	email     varchar(100)          not null,
 	type      enum('A','B','C','D') not null default 'D',
 	status    enum('0','1','2')     not null default '0',
+	desc_user text                           default '',
 	ins_dt    datetime                       default '0',
 	ins_id    int(10) unsigned               default '0',
 	upd_dt    datetime                       default '0',
@@ -16,8 +17,9 @@ create table wbp_user (
 	primary key (id) 
 );
 
-insert into wbp_user (username, password, firstname, lastname, email, type, status, ins_dt, ins_id) values
-		     ('admin', 'test', 'Testie', 'Testtest', 'test@test.de', 'A', '1', '2001-03-14 12:15:20', '1');
+insert into wbp_user (username, password, firstname, lastname, email, type, status, desc_user, ins_dt, ins_id) values
+		     ('admin', 'test', 'Testie', 'Testtest', 'test@test.de', 'A', '1', 'den gibs nur einmal ...', 
+		      '2001-03-14 12:15:20', '1');
 
 insert into wbp_user (username, password, firstname, lastname, email, type, status, ins_dt, ins_id) values
                      ('testb', 'test', 'bla1', 'blabla1', 'bla1@bla1.de', 'B', '1', '2001-03-14 12:15:20', '1');
