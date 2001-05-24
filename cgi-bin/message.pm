84c0e9390a40abc520d16f86fa0c0c54317684bc
fugazi <fugazi>
2001-05-24 22:39:05 +0000

    Messages

--- cgi-bin/message.pm
+++ cgi-bin/message.pm
@@ -2,18 +2,85 @@ package message;
 
 use Class::Singleton;
 use base 'Class::Singleton';
+use message_config;
 use vars qw($VERSION);
 use strict;
  
-$VERSION = sprintf "%d.%03d", q$Revision: 1.1 $ =~ /(\d+)\.(\d+)/;
+$VERSION = sprintf "%d.%03d", q$Revision: 1.2 $ =~ /(\d+)\.(\d+)/;
  
 sub parameter {
  
         my $self = shift;
         my $mgr  = shift;
-
-	$mgr->{Template}      = $mgr->{ErrorTmpl};
-	$mgr->{TmplData}{MSG} = $mgr->decode_all("Jau, wir sind im messages bereich ...");
+	
+	if (defined $mgr->{CGI}->param('method') && $mgr->{CGI}->param('method') eq "write") {
+	    $self->message_form($mgr);
+	    return 1;
+	} elsif (defined $mgr->{CGI}->param('method') && $mgr->{CGI}->param('method') eq "confirm") {
+        $mgr->{Template}      = $mgr->{ErrorTmpl};
+        $mgr->{TmplData}{MSG} = $mgr->decode_all("Wollen Sie das wirklich?");
+	    return 1;
+	} else {
+	    $self->message_menu($mgr);
+	    return 1;
+	}
 }
 
+
+
+sub message_form {
+	my $self = shift;
+	my $mgr  = shift;
+
+	my $link = $mgr->{ScriptName}."?action=%s&sid=".$mgr->{Sid};
+
+	$mgr->{Template} = $mgr->{MessageFormTmpl};
+
+	$mgr->{TmplData}{ACTION} = $mgr->{Action};
+	$mgr->{TmplData}{SID} = $mgr->{Sid};
+	$mgr->{TmplData}{FORM} = $mgr->my_url();
+	$mgr->{TmplData}{SUBJECT} = "Betreff: blablabla";
+
+	# zurueck ins Main Menue mit action=start
+	$mgr->{TmplData}{NAV_START} = sprintf($link, "start");
+	$mgr->{TmplData}{NAV_MESSAGE}    = sprintf($link, $mgr->{Action});
+	$link .= "&method=%s";
+	$mgr->{TmplData}{METHOD}    = "confirm";
+
+	$mgr->fill_header;
+
+	1;
+}
+
+
+
+
+sub message_menu {
+
+	my $self = shift;
+	my $mgr  = shift;
+
+	my $link = $mgr->{ScriptName}."?action=%s&sid=".$mgr->{Sid};
+
+	$mgr->{Template} = $mgr->{MessageTmpl};
+
+	# zurueck ins Main Menue mit action=start
+	$mgr->{TmplData}{NAV_START} = sprintf($link, "start");
+
+	$link .= "&method=%s";
+	$mgr->{TmplData}{NAV_MSG_READ}    = sprintf($link, "message","read");
+	$mgr->{TmplData}{NAV_MSG_WRITE}    = sprintf($link, "message","write");
+
+	$mgr->fill_header;
+
+	1;
+}
+
+
+
+
+
 1;
+
+
+
