55bde9a9f41c44c35170dc148ac4da077247355b
fugazi <fugazi>
2001-05-24 22:41:04 +0000

    Configuration

---
+++ cgi-bin/message_config.pm
@@ -0,0 +1,12 @@
+package message_config;
+
+use vars qw($VERSION);
+
+$VERSION = sprintf "%d.%03d", q$Revision: 1.1 $ =~ /(\d+)\.(\d+)/;
+
+$MSG = {
+        NoMessages  => "Sie haben keine Nachrichten.",
+        Unknownerror => "Es ist ein unbekannter Fehler aufgetreten." 
+};
+
+1;
