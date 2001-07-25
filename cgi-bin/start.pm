ff535e7a8240871d69f0c5a748dff30380bcda94
silcan <silcan>
2001-07-25 20:57:51 +0000

    *** empty log message ***

--- cgi-bin/start.pm
+++ cgi-bin/start.pm
@@ -6,15 +6,17 @@ use start_config;
 use vars qw($VERSION $conf);
 use strict;
 
-$VERSION = sprintf "%d.%03d", q$Revision: 1.5 $ =~ /(\d+)\.(\d+)/;
+$VERSION = sprintf "%d.%03d", q$Revision: 1.6 $ =~ /(\d+)\.(\d+)/;
 
 sub parameter {
 
 	my $self = shift;
 	my $mgr  = shift;
 
+	# Wenn wir eine Session Id haben, pruefen wir, ob der User sich ausloggen will.
 	if ($mgr->{Sid}) {
 		if (defined $mgr->{CGI}->param('method') && $mgr->{CGI}->param('method') eq "logout") {
+			# Session killen und die Logoutseite generien.
 			$mgr->{Session}->kill_session($mgr->{CGI}->param('sid'));
 			$mgr->{Sid}            = undef;
 			$mgr->{Template}       = $mgr->{LoginTmpl};
@@ -22,10 +24,12 @@ sub parameter {
 			return 1;
 		}
 
+		# Sonst geben wir die Startseite wieder aus.
 		$self->show_start($mgr);
 		return 1;
 	}
 
+	# Wenn der Startbutton nicht gedrueckt wurde zeigen wir die Loginseite.
 	unless ($mgr->{CGI}->param('start')) {
 		$mgr->{Template}       = $mgr->{LoginTmpl};
         	$mgr->{TmplData}{FORM} = $mgr->my_url();
@@ -37,16 +41,19 @@ sub parameter {
 
 	my $check = 0;
 
+	# Wurde der Username angegeben?
 	unless (defined $username) {
 		$mgr->{TmplData}{NO_USERNAME} = $mgr->decode_all($start_config::MSG->{NoUserName});
 		$check++;
 	}
 
+	# Wurde das Passwort angegeben?
 	unless (defined $password) {
 		$mgr->{TmplData}{NO_PASSWORD} = $mgr->decode_all($start_config::MSG->{NoPassWord});
 		$check++;
 	}
 
+	# Wenn ein Fehler gezaehlt wurde, geben wir die Loginseite mit Fehlern aus.
 	if ($check) {
 		$mgr->{Template}           = $mgr->{LoginTmpl};
         	$mgr->{TmplData}{FORM}     = $mgr->my_url();
@@ -55,11 +62,13 @@ sub parameter {
 		return 1;
 	}
 
+	# Ueberpruefen des Usernames und des Passworts.
 	if ($self->check($mgr, $username, $password)) {
 		$self->show_start($mgr);
 		return 1;
 	}
 
+	# Sonst Startseite ausgeben.
 	$mgr->{Template}       = $mgr->{LoginTmpl};
 	$mgr->{TmplData}{FORM} = $mgr->my_url();
 	$mgr->{TmplData}{MSG}  = $mgr->decode_all($start_config::MSG->{NoUserExist});
@@ -72,9 +81,11 @@ sub check {
 	my $username = shift;
 	my $password = shift;
 
+	# Connecten und nach dem User suchen.
 	my $dbh = $mgr->connect;
-	my $sth = $dbh->prepare(qq{SELECT * FROM $mgr->{UserTable} WHERE username = ?});
-	
+	my $sth = $dbh->prepare(qq{SELECT * FROM $mgr->{UserTable} WHERE username = ? AND status = '1'});
+
+	# Datenbankanfrage durchfuehren.	
 	unless ($sth->execute($username)) {
 		warn sprintf("[Error]: Trouble selecting from [%]. Reason: [%s].",
 			$mgr->{UserTable}, $dbh->errstr);
@@ -82,10 +93,11 @@ sub check {
 	}
 
 	if ($sth->rows == 0) {
-		warn "[Alert]: Searching password for unknown user.";
+		warn "[Alert]: Searching password for unknown user or user inaktivated.";
 		return undef;
 	}
 
+	# Wenn wir hier sind, ahebn wir einen User gefunden und fuellen die Daten.
 	while (my $href = $sth->fetchrow_hashref) {
 		unless ($password eq $href->{password})	{
 			return undef;
@@ -97,6 +109,7 @@ sub check {
 		$mgr->{UserType}      = $href->{type};
 	}
 
+	# Starten der Session.
 	$mgr->{Sid} = $mgr->{Session}->start_session(FIRSTNAME => $mgr->{UserFirstName},
                              			     LASTNAME  => $mgr->{UserLastName},
                              			     USERID    => $mgr->{UserId},
--- cgi-bin/start_config.pm
+++ cgi-bin/start_config.pm
@@ -2,13 +2,13 @@ package start_config;
 
 use vars qw($VERSION);
 
-$VERSION = sprintf "%d.%03d", q$Revision: 1.2 $ =~ /(\d+)\.(\d+)/;
+$VERSION = sprintf "%d.%03d", q$Revision: 1.3 $ =~ /(\d+)\.(\d+)/;
 
 $MSG = {
 	DbError      => "Es ist ein Fehler mit der Datenbank aufgetreten.",
 	NoPassWord   => "Bitte geben Sie ein Passwort ein.",
 	NoUserName   => "Bitte geben Sie einen Username ein.",
-	NoUserExist  => "Es existiert kein User mit diesem Passwort.",
+	NoUserExist  => "Es existiert kein User mit diesem Passwort oder der User ist deaktiviert.",
 	Unknownerror => "Es ist ein unbekannter Fehler aufgetreten." 
 };
 
